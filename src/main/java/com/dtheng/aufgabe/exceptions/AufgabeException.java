be462996810fed09256e3405389e9a740ea3c4cd
Daniel Thengvall <fender5289@gmail.com>
2017-11-25 18:47:54 -0800

    exceptions package

---
+++ src/main/java/com/dtheng/aufgabe/exceptions/AufgabeException.java
@@ -0,0 +1,11 @@
+package com.dtheng.aufgabe.exceptions;
+
+/**
+ * @author Daniel Thengvall <fender5289@gmail.com>
+ */
+public class AufgabeException extends RuntimeException {
+
+	public AufgabeException(String message) {
+		super(message);
+	}
+}
